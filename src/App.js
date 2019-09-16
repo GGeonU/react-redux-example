fca26af9a777bfa326bfd56d748463cb785ef168
mvl3 <mvl3@mvl3s-Mac-mini.local>
2019-09-16 18:54:21 +0900

    add redux-actions lib

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // import CounterContainer from "./containers/CounterContainer";
 // import Todos from "./components/Todos";
 import TodosContainer from "./containers/TodosContainer";
+import Counter from "./components/Counter";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
     <div className="App">
-      <TodosContainer />
+      <CounterContainer />
     </div>
   );
 }
